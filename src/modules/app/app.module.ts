68b3bdbbd5db830c2ed5a02f9a5fda0e353768ea
rza <rezaa.ras1997@gmail.com>
2024-10-20 23:06:14 +0330

    fixing logical error- installing mysql package-adding authmodule to Appmodule

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TypeOrmConfig } from "src/config/typeorm.config";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
   imports: [
@@ -9,8 +10,8 @@ import { TypeOrmConfig } from "src/config/typeorm.config";
       isGlobal: true,
       envFilePath: ".env",
     }),
-    ,
     TypeOrmModule.forRoot(TypeOrmConfig()),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -8,8 +8,24 @@ import {
   Delete,
 } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { ApiConsumes, ApiTags } from "@nestjs/swagger";
+import { FormType } from "src/common/enums/formType.enum";
+import { checkOtpDto, SendOtpDto } from "./dto/otp.dto";
 
 @Controller("auth")
+@ApiTags("Auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post("/sendOtp")
+  @ApiConsumes(FormType.Urlencoded, FormType.Json)
+  sendOtp(@Body() otpDto: SendOtpDto) {
+    return this.authService.sendOtp(otpDto);
+  }
+
+  @Post("/checkOtp")
+  @ApiConsumes(FormType.Urlencoded, FormType.Json)
+  checkOtp(@Body() otpDto: checkOtpDto) {
+    return this.authService.checkOtp(otpDto);
+  }
 }
