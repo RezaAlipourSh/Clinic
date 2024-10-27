bc41a8a798f3d51c8564dddfe7d72d1da664a14b
rza <rezaa.ras1997@gmail.com>
2024-10-27 10:56:58 +0330

    clinic module & otp , test needed..t

--- src/modules/clinic/clinic.module.ts
+++ src/modules/clinic/clinic.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ClinicService } from './clinic.service';
 import { ClinicController } from './clinic.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ClinicEntity } from './entities/clinic.entity';
+import { ClinicOtpEntity } from './entities/clinicOtp.entity';
+import { CategoryEntity } from '../category/entities/category.entity';
+import { S3Service } from '../s3/s3.service';
+import { JwtService } from '@nestjs/jwt';
+import { CategoryService } from '../category/category.service';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([ClinicEntity,ClinicOtpEntity,CategoryEntity])],
   controllers: [ClinicController],
-  providers: [ClinicService],
+  providers: [ClinicService,S3Service,JwtService,CategoryService],
+  exports:[ClinicService,JwtService,S3Service,TypeOrmModule]
 })
 export class ClinicModule {}
--- src/modules/clinic/dto/clinic.dto.ts
+++ src/modules/clinic/dto/clinic.dto.ts
@@ -1,1 +1,20 @@
-export class ClinicDto {}
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { IsMobilePhone, Length } from "class-validator";
+
+export class ClinicSignupDto {
+  @ApiProperty()
+  @Length(3, 45)
+  first_name: string;
+  @ApiProperty()
+  @Length(3, 50)
+  last_name: string;
+  @ApiProperty()
+  @IsMobilePhone("fa-IR", {}, { message: "invalid mobile Format" })
+  mobile: string;
+  @ApiPropertyOptional()
+  address: string;
+  @ApiProperty()
+  cost: string;
+  @ApiProperty()
+  categoryId: number;
+}
