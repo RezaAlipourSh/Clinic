d080f2ace6341d727a421e50567a50919e17cb0b
rza <rezaa.ras1997@gmail.com>
2024-11-01 08:45:38 +0330

    add guard for clinics  - planer methods post-get-delete

--- src/modules/planer/planer.controller.ts
+++ src/modules/planer/planer.controller.ts
@@ -6,11 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  ParseIntPipe,
+  Query,
 } from "@nestjs/common";
 import { PlanerService } from "./planer.service";
 import { ApiConsumes, ApiTags } from "@nestjs/swagger";
 import { PlanerDto } from "./dto/Planer.dto";
 import { FormType } from "src/common/enums/formType.enum";
+import { PaginationDto } from "src/common/dto/pagination.dto";
+import { Pagination } from "src/common/decorator/pagination.decorator";
 
 @Controller("planer")
 @ApiTags("Planer")
@@ -23,23 +27,29 @@ export class PlanerController {
     return this.planerService.create(createPlanerDto);
   }
 
-  // @Get()
-  // findAll() {
-  //   return this.planerService.findAll();
-  // }
+  @Get()
+  @Pagination()
+  findAll(@Query() pagination: PaginationDto) {
+    return this.planerService.findAll(pagination);
+  }
 
   // @Get(":id")
   // findOne(@Param("id") id: string) {
   //   return this.planerService.findOne(+id);
   // }
 
-  // @Patch(":id")
-  // update(@Param("id") id: string, @Body() updatePlanerDto) {
-  //   return this.planerService.update(+id, updatePlanerDto);
-  // }
+  @Get(":id")
+  @Pagination()
+  @ApiConsumes(FormType.Urlencoded, FormType.Json)
+  update(
+    @Param("id", ParseIntPipe) id: number,
+    @Query() paginationDto: PaginationDto
+  ) {
+    return this.planerService.findClinicPlans(+id, paginationDto);
+  }
 
-  // @Delete(":id")
-  // remove(@Param("id") id: string) {
-  //   return this.planerService.remove(+id);
-  // }
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    return this.planerService.remove(+id);
+  }
 }
--- src/modules/planer/planer.service.ts
+++ src/modules/planer/planer.service.ts
@@ -5,18 +5,23 @@ import { PlanerEntity } from "./entities/planer.entity";
 import { PlanerDto } from "./dto/Planer.dto";
 import { ClinicService } from "../clinic/clinic.service";
 import { ClinicEntity } from "../clinic/entities/clinic.entity";
+import { PaginationDto } from "src/common/dto/pagination.dto";
+import {
+  paginationGenerator,
+  paginationSolver,
+} from "src/common/utility/pagination.util";
 
 @Injectable()
 export class PlanerService {
   constructor(
     @InjectRepository(PlanerEntity) private planRepo: Repository<PlanerEntity>,
-    @InjectRepository(ClinicEntity) private clinicRepo: Repository<ClinicEntity>
+    @InjectRepository(ClinicEntity)
+    private clinicRepo: Repository<ClinicEntity>
   ) {}
 
   async create(PlanerDto: PlanerDto) {
     const { clinicId, day_name, day_number, finish_time, start_time } =
       PlanerDto;
-    // await this.clinicService.findOneById(clinicId);
     const clinic = await this.clinicRepo.findOneBy({ id: clinicId });
     if (!clinic) throw new NotFoundException("not Found clinic By Id");
     await this.planRepo.insert({
@@ -29,6 +34,74 @@ export class PlanerService {
 
     return {
       message: "plan created for clinic Successfully",
+    };
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { limit, page, skip } = paginationSolver(
+      paginationDto.page,
+      paginationDto.limit
+    );
+    const [plans, counts] = await this.planRepo.findAndCount({
+      where: {},
+      skip,
+      take: limit,
+      order: { id: "DESC" },
+    });
+
+    return {
+      pagination: paginationGenerator(counts, page, limit),
+      plans,
+    };
+  }
+
+  async findOneById(id: number) {
+    const plan = await this.planRepo.findOneBy({ id });
+    if (!plan) throw new NotFoundException("plan Not Founded");
+    return plan;
+  }
+
+  async findClinicPlans(clinicId: number, paginationDto: PaginationDto) {
+    const { limit, page, skip } = paginationSolver(
+      paginationDto.page,
+      paginationDto.limit
+    );
+    const [plans, counts] = await this.planRepo.findAndCount({
+      where: { clinicId },
+      relations: {
+        clinic: true,
+      },
+
+      select: {
+        id: true,
+        //if( Unknown column distinctAlias) => set id:true in select option
+        day_name: true,
+        start_time: true,
+        finish_time: true,
+        status: true,
+        clinic: {
+          last_name: true,
+          address: true,
+          cost: true,
+        },
+      },
+      skip,
+      take: limit,
+      order: { id: "DESC" },
+    });
+    console.log(paginationGenerator(counts, page, limit), plans);
+
+    return {
+      pagination: paginationGenerator(counts, page, limit),
+      plans,
+    };
+  }
+
+  async remove(id: number) {
+    await this.findOneById(id);
+    await this.planRepo.delete({ id });
+    return {
+      message: "plan deleted",
     };
   }
 }
