c7a7404a43e6a19ca786f80d14caf9c51afd0183
rza <rezaa.ras1997@gmail.com>
2024-11-03 08:59:00 +0330

    reservation Done . transAction Waiting...

--- src/modules/reservation/reservation.controller.ts
+++ src/modules/reservation/reservation.controller.ts
@@ -6,11 +6,12 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from "@nestjs/common";
 import { ReservationService } from "./reservation.service";
 import { ApiConsumes, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { UserAuth } from "src/common/decorator/auth.decorator";
-import { ReservationDateDto, ReserveTimeDto } from "./dto/reserve.dto";
+import { ClinicReservesDto, ReservationDateDto } from "./dto/reserve.dto";
 import { FormType } from "src/common/enums/formType.enum";
 
 @Controller("reservation")
@@ -21,39 +22,42 @@ export class ReservationController {
 
   @Post()
   @ApiOperation({
-    summary: "select visit date - then choose visit time on another Route",
+    summary: "select visit date & time",
   })
   @ApiConsumes(FormType.Urlencoded, FormType.Json)
   create(@Body() createReservationDto: ReservationDateDto) {
     return this.reservationService.create(createReservationDto);
   }
 
-  @Post("/visitTime")
-  @ApiOperation({
-    summary: "select visit time",
-  })
-  @ApiConsumes(FormType.Urlencoded, FormType.Json)
-  visitTime(@Body() ReservationDto: ReserveTimeDto) {
-    return this.reservationService.selectVisitTime(ReservationDto);
+  // @Post("/visitTime")
+  // @ApiOperation({
+  //   summary: "select visit time",
+  // })
+  // @ApiConsumes(FormType.Urlencoded, FormType.Json)
+  // visitTime(@Body() ReservationDto: ReserveTimeDto) {
+  //   return this.reservationService.selectVisitTime(ReservationDto);
+  // }
+
+  @Get("/clinicReserves")
+  @ApiConsumes(FormType.Urlencoded)
+  findAll(@Query() dto: ClinicReservesDto) {
+    return this.reservationService.findAllClinicReserveByDate(dto);
   }
 
-  // @Get()
-  // findAll() {
-  //   return this.reservationService.findAll();
-  // }
-
-  // @Get(":id")
-  // findOne(@Param("id") id: string) {
-  //   return this.reservationService.findOne(+id);
-  // }
+  @Get(":id")
+  @ApiConsumes(FormType.Urlencoded, FormType.Json)
+  findOne(@Param("id") id: string) {
+    return this.reservationService.findOne(+id);
+  }
 
   // @Patch(":id")
   // update(@Param("id") id: string, @Body() updateReservationDto) {
   //   return this.reservationService.update(+id, updateReservationDto);
   // }
 
-  // @Delete(":id")
-  // remove(@Param("id") id: string) {
-  //   return this.reservationService.remove(+id);
-  // }
+  @Delete(":id")
+  @ApiConsumes(FormType.Urlencoded, FormType.Json)
+  remove(@Param("id") id: string) {
+    return this.reservationService.remove(+id);
+  }
 }
